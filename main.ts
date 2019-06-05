d41c0bcc17277d996acbdd202d37e367e5412498
timsuchanek <tim.suchanek@gmail.com>
2019-06-05 14:20:54 +0200

    update lift demo

--- main.ts
+++ main.ts
@@ -1,18 +1,24 @@
 import { Photon } from '@generated/photon'
 
+const photon = new Photon()
+
 async function main() {
-  const photon = new Photon({
-    // debug: true,
-  })
   await photon.connect()
 
-  const result = await photon.authors.create({
-    data: {
-      name: 'Test',
-    },
-  })
+  const result = await photon.blogs.create(
+    {
+      data: {
+        name: 'Photon Blog',
+        // viewCount: 5,
+      },
+    } as any /* to enable TypeScript, remove this any */,
+  )
 
   console.log(result)
+  photon.close()
 }
 
-main().catch(console.error)
+main().catch(e => {
+  console.error(e)
+  photon.close()
+})
